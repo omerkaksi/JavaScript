330745c51015114c8fa9d171e3cb231db8cc9159
omerkaksi <omerkaksi@yahoo.com>
2024-04-05 08:57:09 +0300

    Lesson 22: Listeners cont. toggle, mouseover, classList.

--- js22/js/main.js
+++ js22/js/main.js
@@ -27,13 +27,27 @@ const initApp = () => {
 const div = view.querySelector("div");
 const h2 = div.querySelector("h2");
 view.addEventListener("click", (event) => {
-    event.target.style.backgroundColor ="purple";
+    /* view.classList.add("purple");
+    view.classList.remove("darkblue"); */
+     view.classList.toggle("purple");
+    view.classList.toggle("darkblue");
 })
 div.addEventListener("click", (event) => {
     event.stopPropagation();
-    event.target.style.backgroundColor ="blue";
+    div.classList.toggle("blue");
+    div.classList.toggle("black");
 })
 h2.addEventListener("click", (event) => {
-    event.target.textContent ="Clicked";
-})
-}
+    const myText =event.target.textContent;
+    myText ==="My 2nd View"
+        ? (event.target.textContent ="Clicked")
+        :(event.target.textContent ="My 2nd View");
+});
+const nav=document.querySelector("nav");
+nav.addEventListener("mouseover", (event) => {
+    event.target.classList.add("height100");
+});
+nav.addEventListener("mouseout", (event) =>{
+    event.target.classList.remove("height100");
+});
+};
