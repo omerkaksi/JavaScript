c0ff1f1efc3d2dbfb1e77206cd994dab7f19ae6c
omerkaksi <omerkaksi@yahoo.com>
2024-04-14 17:31:19 +0300

    JavaScript Courses

---
+++ js5/js/main.js
@@ -0,0 +1,14 @@
+//Math methods
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(3.5));
+console.log(Math.ceil(3.5));
+console.log(Math.floor(3.5));
+console.log(Math.pow(2,10));
+console.log(Math.min(3,5,9));
+console.log(Math.max(3,5,9));
+console.log(Math.random());// 0 - 1 arası
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
---
+++ js6/js/main.js
@@ -0,0 +1,13 @@
+
+const anyName="Jonathan"
+
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)));
+
+
+
+console.log(Math.floor(Math.random()*9));
+let x=Math.floor(Math.random()*9);
+console.log(x);
+console.log("omerkaksi".charAt(x)); 
+
+console.log("omer".charAt(Math.floor(Math.random()*4)));
